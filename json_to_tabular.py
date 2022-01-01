5357fb2eeaf9445719b510f15ca90bae51cd9981
Jakub Adamczyk <jakubadamczyk10@gmail.com>
2022-01-01 23:13:17 +0100

    Change CSV format to normalized

--- json_to_tabular.py
+++ json_to_tabular.py
@@ -17,24 +17,17 @@ def jsons_file_to_tabular_file(in_filename: str, out_filename: str, target_forma
     assert target_format in {"csv", "parquet"}
 
     in_filepath = os.path.join(DATA_DIR, in_filename)
-    df = pd.read_json(in_filepath, lines=True)
 
     if target_format == "csv":
         # change single quotes ' to double quotes " in JSONs
-        for col, dtype in zip(df.columns, df.dtypes):
-            if dtype == "object":
-                df[col] = (
-                    df[col]
-                    .astype(str).str
-                    .replace("'", '"')
-                    .replace("True", "true")
-                    .replace("False", "false")
-                    .replace("None", "null")
-                )
+        with open(in_filepath) as file:
+            df = pd.json_normalize(data=[json.loads(line) for line in file])
 
         out_filepath = os.path.join(CSV_DATA_DIR, out_filename)
-        df.to_csv(out_filepath, index=False, sep=";", quoting=csv.QUOTE_NONE, escapechar="\\")
+        df.to_csv(out_filepath, index=False)
     else:
+        df = pd.read_json(in_filepath, lines=True)
+
         out_filepath = os.path.join(PARQUET_DATA_DIR, out_filename)
         df.to_parquet(out_filepath, engine="pyarrow", compression="snappy", index=None)
 
@@ -50,4 +43,3 @@ if __name__ == '__main__':
         print(f"{out_filename} starting")
         jsons_file_to_tabular_file(in_filename, out_filename, "parquet")
         print()
-
